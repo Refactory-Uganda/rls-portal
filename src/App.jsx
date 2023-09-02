e43ee1d020b60cc60e65c99d323b60809e972b5f
Edwin <rugoogamuedwin5@gmail.com>
2023-09-02 12:39:41 +0300

    changing folders into rls

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
-<<<<<<< HEAD
 import React from 'react';
 import Login from "./Components/Pages/Login"
-=======
-
 import CourseContent from './Admin/CourseContent'
-
-
-function App() {
- 
->>>>>>> 8c9b67839063dcfc100ebbb4867cf1ef836ad5a6
 
 function App(props) {
   return (
-<<<<<<< HEAD
     <div>
       <Login/>
     </div>
   );
-=======
     <>
         <CourseContent />
     </>
-  )
->>>>>>> 8c9b67839063dcfc100ebbb4867cf1ef836ad5a6
 }
 
 export default App;
