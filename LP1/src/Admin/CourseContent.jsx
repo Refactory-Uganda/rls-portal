28e0e30029710624f5ca2c8b6a60d75ee6ed0445
sarahannie <sarahndianekwu@gmail.com>
2023-09-02 08:16:11 +0100

    added an editable content

--- LP1/src/Admin/CourseContent.jsx
+++ LP1/src/Admin/CourseContent.jsx
@@ -1,14 +1,28 @@
-
+import { useState } from 'react';
+import ContentEditable from 'react-contenteditable';
 import Button from '../Components/Button';
 import style from './CourseContent.module.css';
 
 const CourseContent = () => {
+  const [content, setContent] = useState('Input content here');
+
+  function handleChange(event) {
+    setContent(event.target.value);
+  }
   return (
     <div>
       <div className={style.body}>
         <Button name='Materials' padding={'15px'} borderRadius={5} borderColor={'#58C5C8'} color={'black'} backgroundColor={'#58C5C8'} />  
         <Button name='Details' padding={'15px'} borderRadius={5} borderColor={'#58C5C8'} color={'black'} backgroundColor={'#58C5C8'}  />
         <Button name='Entrolled' padding={'15px'} borderRadius={5} borderColor={'#58C5C8'} color={'black'} backgroundColor={'purple'}  />
+    </div>
+    <hr style={{background: 'lime',color: 'lime',borderColor: 'lime',height: '3px', width: '100%'}}/>
+    <div className={style.container}>
+            <ContentEditable html={content} onChange={handleChange} style={{width: '710px', height: '500px',  padding: '10px'}}  />
+        <div className={style.btn}>
+            <Button name='RESET' padding={'8px'} borderRadius={10} borderColor={'#58C5C8'} color={'black'} backgroundColor={'#58C5C8'} size={'10px'} mr={'4px'} />  
+            <Button name='SAVE' padding={'8px'} borderRadius={10} borderColor={'purple'} color={'white'} backgroundColor={'purple'} size={'10px'} />
+        </div>
     </div>
     </div>
   )
--- LP1/src/Components/Button.jsx
+++ LP1/src/Components/Button.jsx
@@ -1,9 +1,9 @@
 
 
-const Button = ({name, size, padding, borderRadius, borderColor, color , boxStyle, backgroundColor}) => {
+const Button = ({name, size, padding, borderRadius, borderColor, color , boxStyle, backgroundColor, mr}) => {
   return (
     <div>
-      <button className='btn btn-outline-light btn-floating' type="button" style={{...boxStyle,fontSize: size, padding: padding, borderRadius: borderRadius,  borderColor: borderColor, backgroundColor:backgroundColor, fontWeight: 'bold', color:color}}>{name}</button>
+      <button className='btn btn-outline-light btn-floating' type="button" style={{...boxStyle,fontSize: size, padding: padding, borderRadius: borderRadius,  borderColor: borderColor, backgroundColor:backgroundColor, fontWeight: 'bold', color:color, marginRight:mr}}>{name}</button>
     </div>
   )
 }
