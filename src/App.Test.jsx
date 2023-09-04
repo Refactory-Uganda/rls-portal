f5db7947f51ad389d45452b020f4ec3b2e2c2f4c
Edwin <rugoogamuedwin5@gmail.com>
2023-09-04 21:29:21 +0300

    test

---
+++ src/App.Test.jsx
@@ -0,0 +1,12 @@
+test('Test', () => {
+    expect(true).toBe(true)
+   });
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import MyComponent from './App';
+
+// test('renders the component with the provided text', () => {
+//   const { getByText } = render(<MyComponent text="Hello, World!" />);
+//   const textElement = getByText(/Hello, World!/i);
+//   expect(textElement).toBeInTheDocument();
+// });
