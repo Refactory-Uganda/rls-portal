300745e25b435c2f19812051f9756d47ed8a75d8
OpiniIsaac <isaacopini8@gmail.com>
2023-09-05 12:28:29 +0300

    added the add facilitator route

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
           <Route path="/login" element={<Login/>}/>
           <Route path="/coursecontent" element={<CourseContent/>}/>
           <Route path="/addCourse" element={<AddCourseContent/>}/>
+          <Route path= 'addFacilitator' element={< AddFacilitator/>}/>
         </Routes>
       </BrowserRouter>
       
