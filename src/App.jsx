0dfc77b28b2d8e6838b9ea46cbbf969ab778c11b
kalungirasuli <kalungirasuli495@gmail.com>
2023-09-05 04:22:06 +0300

    fixed-package.json merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Components/Pages/Login";
 import CourseContent from "./Admin/CourseContent";
 import AddCourseContent from "./Admin/AddCourseContent";
+import Landing from "./Components/Pages/Landing"
+import "./index.css"
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Landing/>}></Route>
           <Route path="/login" element={<Login/>}/>
           <Route path="/coursecontent" element={<CourseContent/>}/>
           <Route path="/addCourse" element={<AddCourseContent/>}/>
