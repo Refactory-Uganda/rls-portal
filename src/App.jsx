1b80f0af12fe94c42222e2264274f968b04e6e63
OpiniIsaac <isaacopini8@gmail.com>
2023-09-06 11:27:45 +0300

    corrected the add facililtot route

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,8 @@ function App() {
           <Route path="/login" element={<Login/>}/>
           <Route path="/coursecontent" element={<CourseContent/>}/>
           <Route path="/addCourse" element={<AddCourseContent/>}/>
-          <Route path= 'addFacilitator' element={< AddFacilitator/>}/>
+          <Route path= '/addFacilitator' element={< AddFacilitator/>}/>
+          
         </Routes>
       </BrowserRouter>
       
