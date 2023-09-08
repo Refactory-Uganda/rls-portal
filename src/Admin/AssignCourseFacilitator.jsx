db8c8803f525cf4516d0a848461ec4d959ff01f1
OpiniIsaac <isaacopini8@gmail.com>
2023-09-08 20:26:27 +0300

    fixed teh styling of  assign course facillitator

--- src/Admin/AssignCourseFacilitator.jsx
+++ src/Admin/AssignCourseFacilitator.jsx
@@ -4,10 +4,12 @@ import style from "./AssignCourseFacilitator.module.css";
 const AssignCourseFacilitator = () => {
     return (
         <>
-        <AdminNacHeader label=' Assign Course Facilitator' />
+    <div className={style.assignCourseFacilitator}>
+    <AdminNacHeader label=' Assign Course Facilitator' />
             <input type="text" placeholder="SELECT COURSE"  className={style.input}/>
             <input type="text" placeholder="SELECT FACILITATOR" className={style.input} />
             <button className={style.button}>Save</button>
+    </div>
            
         </>
     );
