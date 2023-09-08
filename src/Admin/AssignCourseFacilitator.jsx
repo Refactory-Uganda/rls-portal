4a17a7e1130a26cfc19955387943f32e7497e57e
OpiniIsaac <isaacopini8@gmail.com>
2023-09-08 20:13:19 +0300

    started on assign course facillitator

--- src/Admin/AssignCourseFacilitator.jsx
+++ src/Admin/AssignCourseFacilitator.jsx
@@ -5,7 +5,10 @@ const AssignCourseFacilitator = () => {
     return (
         <>
         <AdminNacHeader label=' Assign Course Facilitator' />
-            
+            <input type="text" placeholder="SELECT COURSE" />
+            <input type="text" placeholder="SELECT FACILITATOR" />
+            <button>Save</button>
+           
         </>
     );
 }
