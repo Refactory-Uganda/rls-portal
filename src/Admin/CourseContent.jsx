3dab13044a419e6d5eb2714927810592a96b7960
sarahannie <sarahndianekwu@gmail.com>
2023-09-10 14:59:24 +0100

    completed  addcourse, coursecotent page and facilitator modules with their media query

--- src/Admin/CourseContent.jsx
+++ src/Admin/CourseContent.jsx
@@ -15,13 +15,13 @@ const CourseContent = () => {
     
     <div className={style.body}>
       <div className={style.box}>
-        <Button name='MATERIALS' pr={40} pl={40} mr={'8px'} borderRadius={10} borderColor={'#58C5C8'} color={'black'} backgroundColor={'#58C5C8'} />  
-        <Button name='DETAILS'  pr={40} pl={40} mr={'8px'} borderRadius={10} borderColor={'#58C5C8'} color={'black'} backgroundColor={'#58C5C8'}  />
-        <Button name='ENROLLED' pr={40} pl={40} mr={'8px'} borderRadius={10} borderColor={'purple'} color={'white'} backgroundColor={'#693769'}  />
+        <button className={style.button}>MATERIALS</button>
+        <button className={style.button}>DETAILS</button>
+        <button className={style.button2}>ENROLLED </button>
     </div>
     <hr style={{background: '#693769',color:"#693769", borderColor: '#693769',height: '3px', width: '100%'}}/>
     <div className={style.container}>
-            <ContentEditable html={content} onChange={handleChange} style={{width: '710px', height: '500px',  padding: '10px'}}  />
+            <ContentEditable html={content} onChange={handleChange} className={style.element}  />
         <div className={style.btn}>
             <Button name='RESET' padding={'8px'}  borderRadius={8} borderColor={'#58C5C8'} color={'black'} backgroundColor={'#58C5C8'} size={'10px'} mr={'4px'} />  
             <Button name='SAVE' padding={'8px'} borderRadius={8} borderColor={'purple'} color={'white'} backgroundColor={'#693769'} size={'10px'} />
