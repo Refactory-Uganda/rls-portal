09de08ff7025098fb7c507687b5f7dc440ebdc18
kalungirasuli <kalungirasuli495@gmail.com>
2023-09-11 14:34:09 +0300

    navbar

--- src/Admin/AdminLandingPage.jsx
+++ src/Admin/AdminLandingPage.jsx
@@ -1,5 +1,6 @@
 
 // importing react reactor dom
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 // importing section compontents
 import Nav from "../Components/MicroComponents/NavbarAdmin/AdminNavbar"
 import styles from "./AdminLandingPage.module.css"
@@ -11,7 +12,7 @@ function Admin(){
             <Nav/>
             {/* the view content section tis to arranged here the router element*/}
 
-            <div className={styles.ViewContainer}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro rerum odit qui eligendi enim commodi itaque esse dignissimos aut officiis.
+            <div className={styles.ViewContainer}>
             <div className={styles.ChartandNotifications}>2</div>
             </div>
             {/* the chart and notification content is to arranged here */}
--- src/Components/MicroComponents/NavbarAdmin/AdminNavbar.jsx
+++ src/Components/MicroComponents/NavbarAdmin/AdminNavbar.jsx
@@ -13,21 +13,21 @@ function AdminNavBar(){
                     </div>
             </div>
             <div className="md:flex flex-col justify-around m-0 w-[100%]" id={styles.icons}>
-             <a href="" className="px-1 py-2 flex " id={styles.Navicons}>
+             <a href="" className="px-1 py-2 flex m-1 rounded-0.5sm  " id={styles.Navicons}>
                 <img src="/img/person.svg" alt="" />
                 <p  className="text-white m-0 px-1 hidden " id={styles.NavText}>Account</p>
              </a>
-             <a href="" className="px-1 py-2 hover:bg-white flex " id={styles.Navicons}>
+             <a href="" className="px-1 py-2 hover:bg-white flex m-1 rounded-0.5sm  " id={styles.Navicons}>
                 <img src="/img/school.svg" alt="" />
-                <p className="text-white m-0 px-1 hidden ">Student</p>
+                <p className="text-white m-0 px-1 hidden "  id={styles.NavText}>Student</p>
              </a>
-             <a href="" className="px-1 py-2 hover:bg-white flex " id={styles.Navicons}>
+             <a href="" className="px-1 py-2 hover:bg-white flex m-1 rounded-0.5sm  " id={styles.Navicons}>
                 <img src="/img/group.svg" alt="" />
-                <p className="text-white m-0 px-1 hidden ">Facilitator</p>
+                <p className="text-white m-0 px-1 hidden "  id={styles.NavText}>Facilitator</p>
              </a>
-             <a href=" " className="px-1 py-2 hover:bg-white flex " id={styles.Navicons}>
+             <a href=" " className="px-1 py-2 hover:bg-white flex m-1 rounded-0.5sm  " id={styles.Navicons}>
                 <img src="/img/book.svg" alt="" />
-                <p className="text-white m-0 px-1 hidden ">Courses</p>
+                <p className="text-white  px-1 hidden md:m-0 "  id={styles.NavText}>Courses</p>
              </a>
             </div>
             
