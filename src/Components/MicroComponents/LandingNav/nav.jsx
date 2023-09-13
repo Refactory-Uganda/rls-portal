da36c91df53340221748d7d31523af4e7bc3fa87
kalungirasuli <kalungirasuli495@gmail.com>
2023-09-13 23:15:51 +0300

    refactoring

---
+++ src/Components/MicroComponents/LandingNav/nav.jsx
@@ -0,0 +1,12 @@
+"use client"
+import style from "./style.module.css";
+function Navbar(){
+    return(
+        <>
+<h1 id={style.h1}> The nav</h1>
+        
+        </>
+    )
+}
+
+export default Navbar
