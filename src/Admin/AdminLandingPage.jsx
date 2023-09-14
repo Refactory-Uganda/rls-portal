1ed40a1fae9dd5e06ff17149a780b6dbd34a7f1a
Edwin <rugoogamuedwin5@gmail.com>
2023-09-14 15:55:32 +0300

    sidebar change

--- src/Admin/AdminLandingPage.jsx
+++ src/Admin/AdminLandingPage.jsx
@@ -5,12 +5,13 @@ import { Outlet } from "react-router-dom"
 import Nav from "../Components/MicroComponents/NavbarAdmin/AdminNavbar"
 import styles from "./AdminLandingPage.module.css"
 import Piechart from "../Components/MicroComponents/PieChart/PieChart"
+import SideBar from "../Components/MicroComponents/NewNav/navBar"
 function Admin(){
     return(
         <>
         <div className={styles.AdminDashBoard}>
             {/* the navbar selection is to be arranged here */}
-            <Nav/>
+            <SideBar/>
             {/* the view content section tis to arranged here the router element*/}
 
             <div className={styles.ViewContainer}>
