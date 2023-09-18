ca8dd30962968fe6cfee9f5849af489948cdb7cb
sarahannie <sarahndianekwu@gmail.com>
2023-09-18 09:26:56 +0100

    readjusted sidebar

--- src/Admin/AdminLandingPage.jsx
+++ src/Admin/AdminLandingPage.jsx
@@ -3,18 +3,22 @@
 // importing section compontents
 import { Outlet } from "react-router-dom"
 import Nav from "../Components/MicroComponents/NavbarAdmin/AdminNavbar"
-import styles from "./AdminLandingPage.module.css"
+import styles from "./AdminLandingPage.module.css";
+import Navbar from '../Components/MicroComponents/NewNav/navBar.jsx'
+import PieChart from "../Components/MicroComponents/PieChart/PieChart";
 function Admin(){
     return(
         <>
         <div className={styles.AdminDashBoard}>
             {/* the navbar selection is to be arranged here */}
-            <Nav/>
+            <Navbar />
             {/* the view content section tis to arranged here the router element*/}
 
             <div className={styles.ViewContainer}>
                 <Outlet/>
-            <div className={styles.ChartandNotifications}>2</div>
+            <div className={styles.ChartandNotifications}>
+                <PieChart />
+            </div>
             </div>
             {/* the chart and notification content is to arranged here */}
             
