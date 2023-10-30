a3e93fad7ca069f2ce8442bba1087cb99085da06
Kalungirasuli <kalungirasuli@debian-BULLSEYE-live-builder-AMD64>
2023-10-30 08:06:51 -0400

    front

--- src/Admin/ViewAndAddFacilitors.jsx
+++ src/Admin/ViewAndAddFacilitors.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import axios from 'axios';
 import ViewAddFacilitatorCss from '../Admin/ViewAndAddFacilitators.module.css';
 import { FaEdit, FaEllipsisV, FaTrash } from 'react-icons/fa';
