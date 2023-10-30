253f400ff8e7099b7edc174e6fb8bd61f27e31cf
OpiniIsaac <isaacopini8@gmail.com>
2023-10-30 13:28:07 +0300

    added delete functionality

--- src/Admin/ViewAndAddFacilitors.jsx
+++ src/Admin/ViewAndAddFacilitors.jsx
@@ -18,13 +18,24 @@ function ViewAndAddFacilitors() {
     }
 
     fetchData();
-  },);
+  },[]);
 
+  const handleDelete = async (id) => {
+    try {
+      // Send a DELETE request to your API to delete the facilitator
+      await axios.delete(`http://localhost:5000/facilitator/${id}`);
+      // Update the list of facilitators (you might want to handle this more gracefully)
+      setFacilitators(facilitators.filter((facilitator) => facilitator.id !== id));
+      alert("Data deleted successfully");
+    } catch (error) {
+      console.error('Error deleting facilitator:', error);
+    }
+  };
   return (
     <>
       <div className={`main grid grid-cols-1 p-3 sm2:grid-cols-2 md2:grid-cols-3 gap-3 lg:grid-cols-2 xl:grid-cols-3 ${ViewAddFacilitatorCss.bg}`}>
         {facilitators.map((facilitator) => (
-          <div className="courseCard rounded-1sm p-4 flex flex-col justify-between" key={facilitator.id}>
+          <div className="courseCard rounded-1sm p-4 flex flex-col justify-between" key={facilitator._id}>
             <div id={ViewAddFacilitatorCss.box}>
               <div className="flex justify-center">
                 <img src={`public${facilitator.image}`} alt="img" className={ViewAddFacilitatorCss.image} />
@@ -37,7 +48,7 @@ function ViewAndAddFacilitors() {
                     <ul className="dropdown-menu bg-bluegreen" style={{ minWidth: '5px', backgroundColor: '#673467' }}>
                       <li>
                         <a className="dropdown-item" href="#">
-                          <FaTrash color="#58C5C8" />
+                          <FaTrash onClick={() => handleDelete(facilitator._id)}color="#58C5C8" />
                         </a>
                       </li>
                       <hr style={{ color: '#fff' }}></hr>
