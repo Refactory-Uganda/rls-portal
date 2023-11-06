67bec61a9a2b85dda08601ee8c56fc7d70251ea3
OpiniIsaac <isaacopini8@gmail.com>
2023-11-06 15:14:58 +0300

    started on forum

---
+++ src/Students/Forum.jsx
@@ -0,0 +1,33 @@
+
+import LandingPageNavBar from '../Components/Pages/Student/LandingPageNavBar'
+import style from './forum.module.css'
+export default function Forum() {
+  return (
+    <>
+     <div className="w-full flex items-start justify-center">
+      {/* Left side components */}
+
+      <div className="hidden lg:flex items-center justify-center flex-col p-4 w-0 md:w-1/4 sticky left-0 top-16">
+        <ProfileCom />
+        <SkillCom />
+      </div>
+
+      {/* Center side components */}
+
+      <div className="flex items-center justify-center flex-col  p-3 w-full lg:w-1/2">
+        <Story />
+        <NewpostUploader />
+        <Post />
+      </div>
+
+      {/* Right side components */}
+
+      <div className="hidden lg:flex items-center justify-center flex-col  p-3 w-1/4 sticky right-0 top-16">
+        <Recent />
+      </div>
+    </div>
+   
+ 
+    </>
+  )
+}
