604edbee3f64bc41f451a44ba5aad47f8588c2a9
eric <maneeric20@gmail.com>
2023-11-15 10:04:02 +0300

    json

--- src/Students/ViewMaterial.jsx
+++ src/Students/ViewMaterial.jsx
@@ -8,6 +8,11 @@ import LandingPageNavBar from '../Components/Pages/Student/LandingPageNavBar';
 import axios from "axios"
 
 const ViewMaterial = () => {
+  const materialData = [
+    { type: 'video', url: 'https://www.youtube.com/watch?v=AagjAUE8U8s', title: 'Introduction to CSS', icon: <BiCaretRightCircle size={50} /> },
+    { type: 'pdf', url: 'pdf_url_here', title: 'Introduction To HTML', icon: <BiSave size={50} /> },
+    { type: 'word', url: 'word_url_here', title: 'Introduction To Javascript', icon: <BiFile size={50} /> },
+  ];
   const [courseMaterial, setCourseMaterial] = useState([]);
 
   useEffect(() => {
@@ -30,28 +35,25 @@ const ViewMaterial = () => {
       <LandingPageNavBar />
       <Accordion defaultActiveKey="0" className={styles.accordion}>
         <h1>Course Material</h1>
-        {courseMaterial.map((section, index) => (
-          <Accordion.Item key={index} eventKey={index.toString()}>
-            <Accordion.Header>{section.module_name}</Accordion.Header>
-            <Accordion.Body>
-              {section.video && (
-                <MaterialComponent
-                  icon={<BiCaretRightCircle size={50} />}
-                  title="Video Material"
-                  url={section.video}
-                />
-              )}
-              {section.text && (
-                <MaterialComponent
-                  icon={<BiSave size={50} />}
-                  title="Text Material"
-                  url={section.text}
-                />
-              )}
-            </Accordion.Body>
-          </Accordion.Item>
-        ))}
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Introduction</Accordion.Header>
+          <Accordion.Body>
+            {materialData.map((material, index) => (
+              <MaterialComponent key={index} icon={material.icon} title={material.title} url={material.url} />
+            ))}
+          </Accordion.Body>
+        </Accordion.Item>
       </Accordion>
+      <Accordion defaultActiveKey="0" className={styles.accordion2}>
+      <Accordion.Item eventKey="0">
+          <Accordion.Header>Object Oriented Programming</Accordion.Header>
+          <Accordion.Body>
+            {materialData.map((material, index) => (
+              <MaterialComponent key={index} icon={material.icon} title={material.title} url={material.url} />
+            ))}
+          </Accordion.Body>
+        </Accordion.Item>
+        </Accordion>
     </>
   );
 };
