b4b328a3c0abbdb2f96c32306679f3ef8ae738d9
Isaac Kalumba <ikalumba221@gmail.com>
2024-10-27 17:45:47 +0300

    updates

---
+++ src/Admin/CourseContentView.jsx
@@ -0,0 +1,13 @@
+import react from "@vitejs/plugin-react-swc";
+
+function CourseContentView() {
+  return (
+    <div className="container">
+      <div className="card course-header-header"></div>
+      <div className="course-what-to-learn"></div>
+      <div className="course-content-list"></div>
+    </div>
+  );
+}
+
+export default CourseContentView;
