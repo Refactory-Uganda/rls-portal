9b31677a038cd5de7efeb731db2b4c7aba6f3dcb
Daphine Nambafu <daphinenambafu@gmail.com>
2024-11-02 13:45:09 +0300

    working on updatequiz

--- src/Admin/CourseContentView.jsx
+++ src/Admin/CourseContentView.jsx
@@ -1,72 +1,38 @@
-import React, { useEffect, useState } from "react";
-import api from "../services/api";
-import "../../src/assets/css/courseDetails.css";
-import "../../src/assets/css/courseContentView.css";
-import TopicsList from "./TopicsList";
+import React, { useState } from "react";
 import ContentList from "./ContentList";
+import EditQuiz from "./EditQuiz";
 
-const CourseContentView = ({
-  selectedCourse,
-  setSelectedCourse,
-  onDelete,
-  setView,
-  error,
-  setError,
-}) => {
+const CourseContentView = ({ selectedCourse, setView }) => {
   const [lessonToview, setLessonToView] = useState(null);
+  const [isEditQuizModalOpen, setIsEditQuizModalOpen] = useState(false);
 
   const handleBackClick = () => {
-    // setSelectedCourse(null); // Reset the selected course
-    setView("details"); // Change the view to "list" to show the course list
+    setView("details");
   };
 
-  // const handleDelete = async () => {
-  //   if (!window.confirm("Are you sure you want to delete this course?")) {
-  //     return;
-  //   }
-
-  //   try {
-  //     console.log("Deleting course with ID:", selectedCourse.id);
-  //     await api.delete(`/courses/${selectedCourse.id}`); // Ensure this endpoint matches your NestJS controller
-  //     onDelete(selectedCourse.id); // Update UI after deletion
-  //   } catch (error) {
-  //     console.error("Error deleting course:", error);
-  //     alert("Failed to delete the course. Please try again.");
-  //   }
-  // };
-
-  // const handleEdit = () => {
-  //   setView("edit");
-  // };
   const handleViewLessonContent = (lesson) => {
     setLessonToView(lesson);
   };
-  console.log(selectedCourse);
+
+  const toggleEditQuizModal = () => setIsEditQuizModalOpen(!isEditQuizModalOpen);
+
   return (
     <div className="container mx-auto my-8">
       <div className="container courseList-btn-container">
-        <button
-          className="btn btn-primary action-btn"
-          onClick={handleBackClick}
-        >
+        <button className="btn btn-primary action-btn" onClick={handleBackClick}>
           <i className="bi bi-arrow-left"></i> Back to Courses Details
         </button>
       </div>
 
       <div className="course-and-topics-details-container row d-flex align-items-stretch">
-        {/* Content view window */}
         <div className="card col-md-8 d-flex">
-          {lessonToview ? (
+          {lessonToview && (
             <>
               <h3>{lessonToview.title}</h3>
               <p>{lessonToview.text}</p>
             </>
-          ) : (
-            ""
           )}
         </div>
-
-        {/* Topics Container */}
         <div className="course-topics-container card col-md-4 d-flex">
           <ContentList
             selectedCourse={selectedCourse}
@@ -76,28 +42,38 @@ const CourseContentView = ({
           />
         </div>
       </div>
+
       <div className="container quiz-btn-container">
-        {lessonToview ? (
+        {lessonToview && (
           <>
             <a
               href="#"
               className="btn btn-primary action-btn"
-              // onClick={() => setView("createCourse")}
+            // onClick={() => setView("createCourse")}
             >
               Take Lesson Quiz
             </a>
             <a
               href="#"
-              className="btn btn-primary secondary-action-btn"
-              // onClick={() => setView("createCourse")}
+              className="btn btn-primary action-btn"
+              onClick={(e) => {
+                e.preventDefault();
+                toggleEditQuizModal();
+              }}
             >
               Edit Quiz
             </a>
           </>
-        ) : (
-          ""
         )}
       </div>
+
+      {isEditQuizModalOpen && (
+        <EditQuiz
+          isEditModalOpen={isEditQuizModalOpen}
+          toggleQuizModal={toggleEditQuizModal}
+          quizData={lessonToview.quiz}
+        />
+      )}
     </div>
   );
 };
