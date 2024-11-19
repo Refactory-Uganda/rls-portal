2fb14e39d7af6ea87916341db84564c54171c757
Isaac Kalumba <ikalumba221@gmail.com>
2024-11-19 17:55:05 +0300

    UI updates

--- src/Facilitator/CourseList.jsx
+++ src/Facilitator/CourseList.jsx
@@ -1,37 +1,47 @@
 import React from "react";
-import CourseCard from "./FCourseCard";
+import CourseCard from "../Components/LCourseCard";
 
-const CourseList = ({ courses = [], setSelectedCourse, view, setView }) => {
+const CourseView = ({ courses, setSelectedCourse, view, setView }) => {
   // Handle course card click to display course details
   const handleCourseClick = (course) => {
     setSelectedCourse(course);
     setView("details");
   };
-
   console.log(courses);
-
   return (
     <div className="container">
-      {/* Check if courses is available and is an array */}
-      {Array.isArray(courses) && courses.length === 0 ? (
-        <p>No courses available.</p>
+      {/* <div className="courseList-btn-container">
+        <a
+          href="#"
+          className="btn action-btn"
+          onClick={() => setView("createCourse")}
+        >
+          Create new course
+        </a>
+        <a
+          href="#"
+          className="btn secondary-action-btn"
+          // onClick={() => setView("createCourse")}
+        >
+          Open drafts
+        </a>
+      </div> */}
+      {/* Optional: Add a "Create Course" button here */}
+      {courses.length === 0 ? (
+        <p>Loading courses...</p>
       ) : (
-        <div className="row row">
-           {/* <div className="row row-cols-3 row-cols-lg-3 g-2 g-lg-3"> */}
-          {Array.isArray(courses) &&
-            courses.map((course) => (
-              <CourseCard
-                key={course.id}
-                course={course}
-                onClick={() => handleCourseClick(course)}
-              />
-            ))}
+        <div className="row row-cols-3 row-cols-lg-3 g-1 g-lg-3">
+          {courses.map((course) => (
+            <CourseCard
+              key={course.id}
+              course={course}
+              onClick={() => handleCourseClick(course)}
+            />
+          ))}
         </div>
       )}
     </div>
   );
 };
 
-export default CourseList;
-
-
+export default CourseView;
