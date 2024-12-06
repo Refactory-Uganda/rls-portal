37508cbe2db532de488aa4644dd4ede42eb35341
Daphine Nambafu <daphinenambafu@gmail.com>
2024-12-06 13:51:35 +0300

    correct a path in the adminLogin file

--- src/Auth/users/AdminLogin.jsx
+++ src/Auth/users/AdminLogin.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import LoginHeader from "./login/LoginHeader";
-import LoginForm from "./login/LoginForm";
+import LoginHeader from "../users/Login/LoginHeader";
+import LoginForm from "../users/Login/LoginForm";
 function AdminLogin() {
   return (
     <>
